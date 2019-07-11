bee299d153a0fc7ca5e0d570b03042bd7ebbfe8f
bae jiun, maybe <alice.maydev@gmail.com>
2019-07-12 07:16:20 +0900

    Update combine

--- combine.py
+++ combine.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 import json
 import argparse
 from pathlib import Path
@@ -5,12 +7,32 @@ from pathlib import Path
 parser = argparse.ArgumentParser(description='Combine results to json file')
 parser.add_argument('--root', required=False, default='/home/anears/Data/RNDGC', type=str)
 parser.add_argument('--team_ID', default='team1', type=str)
+parser.add_argument('--gt', type=str)
 args = parser.parse_args()
 
 root = Path(args.root)
 
 with open(f't1_res_{args.team_ID}.json', 'w', encoding='utf-8') as f:
-    json.dump({
+    results = {
         'track1_results': [json.loads(open(json_file).read()) for json_file in map(str, root.glob('*.json'))]
-    }, f, ensure_ascii=False, indent='\t')
+    }
+    json.dump(results, f, ensure_ascii=False, indent='\t')
 
+if args.gt:
+    with open(args.gt) as f:
+        ground_truths = json.load(f)
+    
+    preds = results['track1_results']
+    gts = ground_truths['track1_GT']
+
+    w = [5, 1, 1, 1, 1, 1]
+    al = []
+
+    for idx in range(len(gts)):
+        gt = gts[idx]['objects']
+        p = preds[idx]['objects']
+        dist = [w[i]*((gt[i]-p[i])**2) for i in range(6)]
+        print(gts[idx]['id'], math.sqrt(sum(dist)), math.sqrt(sum(dist)) > 30 or '')
+        al.append(math.sqrt(sum(dist)))
+
+    print('mean', np.array(al).mean())
--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ for f in $ROOT/t1_video/*; do
 
     if [ -d "$f" ]; then
         cd detector;
-        python main.py inference --dataset $ROOT/t1_video/$SEQ --dest $ROOT/results/$SEQ --classes coco --no-cache --jump $JUMP > /dev/null 2>&1;
+        python main.py inference --dataset $ROOT/t1_video/$SEQ --config configs/config.aigc.json --dest $ROOT/results/$SEQ --classes coco --no-cache --jump $JUMP > /dev/null 2>&1;
         cd ../identifier;
         python main.py --dataset $ROOT/t1_video/$SEQ --support $ROOT/results/$SEQ/mask_rcnn_coco --support-only --type detection --dest $ROOT/results --no-sequence --jump $JUMP > /dev/null 2>&1;
         cd ..;
