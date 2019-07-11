307ec680cda6e44f93c896b92a0fd8e6350c7062
jiun <jiun.maydev@gmail.com>
2019-07-12 02:59:40 +0900

    Update combine

--- combine.py
+++ combine.py
@@ -1,16 +1,16 @@
-import glob
-import argparse
 import json
+import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Combine results to json file')
 parser.add_argument('--root', required=False, default='/home/anears/Data/RNDGC', type=str)
 parser.add_argument('--team_ID', default='team1', type=str)
-arguments = parser.parse_args()
+args = parser.parse_args()
 
-results = {"track1_results":[]}
-for jsonPath in sorted(glob.glob('*.json')):
-	res = json.loads(open(jsonPath).read())
-	results["track1_results"].append(res)
+root = Path(args.root)
 
-with open('t1_res_'+arguments.team_ID+'.json','w', encoding='utf-8') as fp:
-	json.dump(results, fp, ensure_ascii=False, indent='\t')
+with open(f't1_res_{args.team_ID}.json', 'w', encoding='utf-8') as f:
+    json.dump({
+        'track1_results': [json.loads(open(json_file).read()) for json_file in map(str, root.glob('*.json'))]
+    }, f, ensure_ascii=False, indent='\t')
+
