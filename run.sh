9be630fd999b94dbc70bde4ba5a4289ade658eca
bae jiun, maybe <alice.maydev@gmail.com>
2019-07-12 00:24:47 +0900

    Update script for sequence

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
-cd detector;
-python main.py inference --dataset /home/cvlab/datasets/AIGC/t1_video/$1 --dest /home/cvlab/datasets/AIGC/results/$1 --weights weights/mask_rcnn_coco.h5 --classes coco --no-cache --jump 10;
-cd ../identifier;
-python main.py --dataset /home/cvlab/datasets/AIGC/t1_video/$1 --support /home/cvlab/datasets/AIGC/results/$1/mask_rcnn_coco --support-only --type detection --dest /home/cvlab/datasets/AIGC/results --no-sequence --jump 10;
-cd ..;
-python evaluate.py --root /home/cvlab/datasets/AIGC  --sequence $1
+ROOT=$1
+JUMP=10
+
+echo $ROOT
+echo $JUMP
+
+for f in $ROOT/videos/*; do
+    echo $f
+    if [ -d "$f" ]; then
+        cd detector;
+        python main.py inference --dataset $ROOT/videos/$f --dest $ROOT/results/$f --classes coco --no-cache --jump $JUMP > /dev/null 2>&1;
+        cd ../identifier;
+        python main.py --dataset $ROOT/videos/$f --support $ROOT/results/$f/mask_rcnn_coco --support-only --type detection --dest $ROOT/results --no-sequence --jump $JUMP > /dev/null 2>&1;
+        cd ..;
+        python evaluate.py --root /home/cvlab/datasets/AIGC  --sequence $f
+    fi
+done
