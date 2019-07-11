640d26cd76c53d162dd6f3a9c76cc6a0e1afd032
bae jiun, maybe <alice.maydev@gmail.com>
2019-07-12 00:08:19 +0900

    Update workspace

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ cd detector;
 python main.py inference --dataset /home/cvlab/datasets/AIGC/t1_video/$1 --dest /home/cvlab/datasets/AIGC/results/$1 --weights weights/mask_rcnn_coco.h5 --classes coco --no-cache --jump 10;
 cd ../identifier;
 python main.py --dataset /home/cvlab/datasets/AIGC/t1_video/$1 --support /home/cvlab/datasets/AIGC/results/$1/mask_rcnn_coco --support-only --type detection --dest /home/cvlab/datasets/AIGC/results --no-sequence --jump 10;
+cd ..;
 python evaluate.py --root /home/cvlab/datasets/AIGC  --sequence $1
-
