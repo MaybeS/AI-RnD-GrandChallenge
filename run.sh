338b55cb1fedc9f332a719757d5b75c9060d7f7d
bae jiun, maybe <alice.maydev@gmail.com>
2019-07-12 00:35:32 +0900

    Update dirpaht

--- run.sh
+++ run.sh
@@ -5,13 +5,14 @@ echo $ROOT
 echo $JUMP
 
 for f in $ROOT/videos/*; do
-    echo $f
+    SEQ=$(dirname $f)
+
     if [ -d "$f" ]; then
         cd detector;
-        python main.py inference --dataset $ROOT/videos/$f --dest $ROOT/results/$f --classes coco --no-cache --jump $JUMP > /dev/null 2>&1;
+        python main.py inference --dataset $ROOT/videos/$SEQ --dest $ROOT/results/$SEQ --classes coco --no-cache --jump $JUMP > /dev/null 2>&1;
         cd ../identifier;
-        python main.py --dataset $ROOT/videos/$f --support $ROOT/results/$f/mask_rcnn_coco --support-only --type detection --dest $ROOT/results --no-sequence --jump $JUMP > /dev/null 2>&1;
+        python main.py --dataset $ROOT/videos/$SEQ --support $ROOT/results/$SEQ/mask_rcnn_coco --support-only --type detection --dest $ROOT/results --no-sequence --jump $JUMP > /dev/null 2>&1;
         cd ..;
-        python evaluate.py --root /home/cvlab/datasets/AIGC  --sequence $f
+        python evaluate.py --root /home/cvlab/datasets/AIGC  --sequence $SEQ
     fi
 done
