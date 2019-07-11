d4a4aac9d23aa02c3115b0a1b8f928ff852ebfce
bae jiun, maybe <alice.maydev@gmail.com>
2019-07-12 00:02:33 +0900

    Update sequence var

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd detector;
-python main.py inference --dataset /home/cvlab/datasets/AIGC/t1_video/t1_video_00001 --dest /home/cvlab/datasets/AIGC/results/t1_video_00001 --weights weights/mask_rcnn_coco.h5 --classes coco --no-cache --jump 10;
+python main.py inference --dataset /home/cvlab/datasets/AIGC/t1_video/$1 --dest /home/cvlab/datasets/AIGC/results/$1 --weights weights/mask_rcnn_coco.h5 --classes coco --no-cache --jump 10;
 cd ../identifier;
-python main.py --dataset /home/cvlab/datasets/AIGC/t1_video/t1_video_00001 --support /home/cvlab/datasets/AIGC/results/t1_video_00001/mask_rcnn_coco --support-only --type detection --dest /home/cvlab/datasets/AIGC/results --no-sequence --jump 10;
-python evalate.py --root /home/cvlab/datasets/AIGC  --sequence t1_video_00001
+python main.py --dataset /home/cvlab/datasets/AIGC/t1_video/$1 --support /home/cvlab/datasets/AIGC/results/$1/mask_rcnn_coco --support-only --type detection --dest /home/cvlab/datasets/AIGC/results --no-sequence --jump 10;
+python evalate.py --root /home/cvlab/datasets/AIGC  --sequence $1
 
